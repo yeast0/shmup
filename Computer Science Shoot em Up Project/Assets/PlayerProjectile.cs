2835f46b224bd96e77b0137f6ee69c6659ea8b3c
yeast0 <jackyeates200420042004@outlook.com>
2023-01-25 05:57:24 +0000

    Enemy Animations / import
    added enemies, a title screen and starting / quitting functionality

--- Computer Science Shoot em Up Project/Assets/PlayerProjectile.cs
+++ Computer Science Shoot em Up Project/Assets/PlayerProjectile.cs
@@ -13,7 +13,7 @@ public class PlayerProjectile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 3);
     }
 
     // Update is called once per frame
---
+++ Computer Science Shoot em Up Project/Assets/Scripts/menuControl.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuControl : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
